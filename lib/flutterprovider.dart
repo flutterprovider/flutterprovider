6fa901879813051de87faad1ac8ecd05281b5402
Aarif Husain <mail@aarifhusain.com>
2025-10-26 14:43:06 +0530

    Update flutterprovider.dart

--- lib/flutterprovider.dart
+++ lib/flutterprovider.dart
@@ -7,7 +7,8 @@ export 'package:flutter/material.dart';
 export 'package:flutter_svg/svg.dart';
 export 'package:google_fonts/google_fonts.dart';
 
-/// aaspas
+/// [aaspas]
+/// Color Box file
 export 'src/aaspas/aaspas_color_box.dart';
 export 'src/aaspas/details_widgets/service_details.dart';
 export 'src/aaspas/details_widgets/shop_details.dart';
@@ -19,10 +20,16 @@ export 'src/aaspas/other_widgets/category_chip_close.dart';
 export 'src/aaspas/other_widgets/item_chip.dart';
 export 'src/aaspas/other_widgets/item_chip_close.dart';
 export 'src/aaspas/other_widgets/widget_heading.dart';
-/// moyo
+
+/// [moyo]
+/// Details Cards
+export "src/moyo/details_widgets/user_service_details.dart";
+/// List Cards
+export "src/moyo/list_cards/personal_info_card.dart";
 export "src/moyo/list_cards/provider_service_list_card.dart";
+export "src/moyo/list_cards/user_address_card.dart";
+export "src/moyo/list_cards/user_expansion_tile.dart";
 export "src/moyo/list_cards/user_interested_provider_list_card.dart";
 export "src/moyo/list_cards/user_service_list_card.dart";
-export "src/moyo/list_cards/personal_info_card.dart";
-export "src/moyo/list_cards/user_address_card.dart";
+/// Color Box file
 export 'src/moyo/moyo_color_box.dart';
